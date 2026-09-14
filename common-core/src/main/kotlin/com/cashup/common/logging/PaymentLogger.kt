018c9ba283c753b21452cbd22fbeda92e9b2f2a8
nahsbytedev <nahsbyte.dev@gmail.com>
2026-09-15 00:53:54 +0700

    feat(common-core): add PaymentLogger interface

---
+++ common-core/src/main/kotlin/com/cashup/common/logging/PaymentLogger.kt
@@ -0,0 +1,13 @@
+package com.cashup.common.logging
+
+interface PaymentLogger {
+    fun debug(tag: String, message: String)
+    fun warn(tag: String, message: String, throwable: Throwable? = null)
+    fun error(tag: String, message: String, throwable: Throwable? = null)
+}
+
+class NoOpPaymentLogger : PaymentLogger {
+    override fun debug(tag: String, message: String) = Unit
+    override fun warn(tag: String, message: String, throwable: Throwable?) = Unit
+    override fun error(tag: String, message: String, throwable: Throwable?) = Unit
+}
